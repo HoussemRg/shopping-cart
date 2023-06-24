fe9ede68b34eb71ec5e8a78a171277dc52217a7c
houssem <houssem.regaieg@etudiant-enit.utm.tn>
2023-06-24 15:38:56 +0100

    this is my second update .juste still to calculate the total and effect the buy

--- shopping-cart.js
+++ shopping-cart.js
@@ -49,10 +49,31 @@ let arrayOfProducts=[
     }
 
 ];
-let nbProducts=0;
-let cart=document.querySelector('.cart');
+//localStorage.clear();
+//localStorage.clear();
+let nbProducts;
+let cart=document.querySelector('.cart-items');
+let totalCart=document.querySelector('.cart');
 let item=document.getElementById('item');
 let productsBuyed=document.getElementById('pd');
+let total=document.querySelector('.total');
+let sum=Number(total.innerHTML);
+
+if(localStorage.getItem('cart')!=null){
+    cart.innerHTML=localStorage.getItem('cart');
+}
+else{
+    cart.innerHTML=`<button class="previous" onclick="hideCart();"><img src="img/Screenshot 2023-06-21 225736.png" alt="" class="previous-img"></button>
+    <div class="cart-title"><h2>Your Cart</h2></div>`;
+}
+if(localStorage.getItem('nbProducts')!=null){
+    nbProducts=localStorage.getItem('nbProducts');
+    productsBuyed.innerHTML=nbProducts;
+}
+else{
+    nbProducts=0;
+    productsBuyed.innerHTML=nbProducts;
+}
 function addToCart(index){
     if(arrayOfProducts[index-1].state==='not-added'){
         cart.innerHTML+=`
@@ -64,30 +85,38 @@ function addToCart(index){
                     <div class="description">
                         <p class="item-title">${arrayOfProducts[index-1].productName}</p>
                         <p class="item-price">${arrayOfProducts[index-1].productPrice}</p>
-                        <input type="number" name="Number" value="1" id="number" min="1">
+                        <input type="number" name="Number" value="1" id="number-${index}" min="1">
                     </div>
                     <button class="delete" onclick="removeProduct(${index});"><img src="img/bxs-trash-alt.png" alt="" class="delete-item"></button>
                     </div>
-                    <div class="total"></div>
-                    <button class="calculate">Buy Now</button>
                     </div>`;
+        let coef=document.querySelector(`#number-${index}`);
+        let numberOfProductsToBuy=0;
+        coef.addEventListener('input',(event)=>{
+            numberOfProductsToBuy=event.target.value;
+        })
+        console.log(numberOfProductsToBuy);
+        
         arrayOfProducts[index-1].state='added';
         nbProducts++;
+        localStorage.setItem('nbProducts',nbProducts);
         productsBuyed.innerHTML=nbProducts;
+        sum+=Number(arrayOfProducts[index-1].productPrice);
+        total.innerHTML=sum;
     }
     else{
         alert('product added');
     }
-    
+    localStorage.setItem('cart',cart.innerHTML);
 }
 
 function showCart(){
-    cart.classList.add('buy');
-    console.log(cart);
+    //cart.classList.add('buy');
+    totalCart.classList.add('buy');
 }
 function hideCart(){
-    cart.classList.remove('buy');
-    console.log(cart);
+    //cart.classList.remove('buy');
+    totalCart.classList.remove('buy');
 }
 
 function removeProduct(index){
@@ -95,5 +124,13 @@ function removeProduct(index){
     item.parentElement.remove();
     arrayOfProducts[index-1].state='not-added';
     nbProducts--;
+    localStorage.setItem('nbProducts',nbProducts);
     productsBuyed.innerHTML=nbProducts;
+    localStorage.setItem('cart',cart.innerHTML);
+    sum-=Number(arrayOfProducts[index-1].productPrice);
+    total.innerHTML=sum;
+    
 }
+
+
+
